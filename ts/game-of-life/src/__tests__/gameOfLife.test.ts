ea156151b6257c6a1d4612f073b2220dd74555aa
Emmanuel Conrardy <econrardy@dicksondata.fr>
2024-07-03 13:18:21 +0200

    add overcrowded

--- ts/game-of-life/src/__tests__/gameOfLife.test.ts
+++ ts/game-of-life/src/__tests__/gameOfLife.test.ts
@@ -4,7 +4,7 @@ describe('GameOfLife', () => {
     });
 
 
-    for (const neighbours of [3, 2]) {
+    for (const neighbours of [2, 3]) {
         it(`Any live cell with ${neighbours} live neighbours lives on to the next generation`, () => {
             const cell = new LiveCell();
 
@@ -12,6 +12,17 @@ describe('GameOfLife', () => {
 
             const expectedLiveCell = new LiveCell();
             expect(nextGeneration).toStrictEqual(expectedLiveCell);
+        });
+    }
+
+
+    for (const neighbours of [4, 5, 6, 7, 8]) {
+        it(`Any live cell with ${neighbours} live neighbours dies, as if by overcrowding`, () => {
+            const cell = new LiveCell();
+
+            const nextGeneration = cell.nextGeneration(neighbours);
+
+            expect(nextGeneration).toBeUndefined();
         });
     }
 });

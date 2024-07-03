5ac533ce75da12ae0dd9507e50f2f2af2fd12bb2
Emmanuel Conrardy <econrardy@dicksondata.fr>
2024-07-03 13:30:49 +0200

    implement underpopulation and dead cell become live

--- ts/game-of-life/src/__tests__/gameOfLife.test.ts
+++ ts/game-of-life/src/__tests__/gameOfLife.test.ts
@@ -1,8 +1,36 @@
+export class LiveCell {
+    public nextGeneration(neighbours: number): DeadCell | LiveCell {
+        if (neighbours === 2 || neighbours === 3) {
+            return this;
+        }
+
+        return new DeadCell();
+    }
+}
+export class DeadCell {
+    public nextGeneration(neighbours: number): DeadCell | LiveCell {
+        if (neighbours === 3) {
+            return new LiveCell();
+        }
+
+        return new DeadCell();
+    }
+}
 describe('GameOfLife', () => {
     it('should be true', () => {
         expect(true).toBe(true);
     });
 
+    for (const neighbours of [0, 1]) {
+        it(`Any live cell with ${neighbours} live neighbours dies, as if caused by underpopulation`, () => {
+            const cell = new LiveCell();
+
+            const nextGeneration = cell.nextGeneration(neighbours);
+
+            const expectedDeadCell = new DeadCell();
+            expect(nextGeneration).toStrictEqual(expectedDeadCell);
+        });
+    }
 
     for (const neighbours of [2, 3]) {
         it(`Any live cell with ${neighbours} live neighbours lives on to the next generation`, () => {
@@ -22,17 +50,17 @@ describe('GameOfLife', () => {
 
             const nextGeneration = cell.nextGeneration(neighbours);
 
-            expect(nextGeneration).toBeUndefined();
+            const expectedDeadCell = new DeadCell();
+            expect(nextGeneration).toStrictEqual(expectedDeadCell); 
         });
     }
+
+    it('Any dead cell with exactly three live neighbours becomes a live cell', () => {
+        const cell = new DeadCell();
+
+        const nextGeneration = cell.nextGeneration(3);
+
+        const expectedLiveCell = new LiveCell();
+        expect(nextGeneration).toStrictEqual(expectedLiveCell);
+    });
 });
-
-export class LiveCell {
-    public nextGeneration(neighbours: number) {
-        if (neighbours === 2 || neighbours === 3) {
-            return this;
-        }
-
-        return undefined;
-    }
-}

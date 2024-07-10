54644d5735357252c6f7f8a043f570ae3ccb2f88
Herve Soulie <herve.soulie@vetup.com>
2024-07-10 13:33:44 +0200

    ajout random seeding

--- csharp/GameOfLife/GameOfLifeApp/Program.cs
+++ csharp/GameOfLife/GameOfLifeApp/Program.cs
@@ -23,70 +23,19 @@
 			string? seedingMethod = Console.ReadLine();
 
 			switch (seedingMethod) {
-				case "":
-				case "1":
-					break;
+                case "":
+                case "1":
+                    seed = RandomSeeding(rows, cols);
+                    break;
 
-				case "2":
-					seed = new Cell[rows, cols];
-					Console.WriteLine("Enter the initial state of the grid:");
-					Console.WriteLine("Use 'X' for live cells and '.' for dead cells");
-					Console.WriteLine("Press enter after each row");
+                case "2":
+                    seed = ManualSeeding(rows, cols);
+                    break;
 
-					for (int i = 0; i < rows; i++) {
-						string? row = Console.ReadLine();
-						for (int j = 0; j < cols; j++) {
-							if (row[j] == 'X' || row[j] == 'x') {
-								// live cell
-								seed[i, j] = new LiveCell();
-							} else {
-								// dead cell
-								seed[i, j] = new DeadCell();
-							}
-						}
-					}
-					break;
-
-				case "3":
-					Console.WriteLine("Enter the seeding map:");
-					seed = new Cell[rows, cols];
-					string? map = Console.ReadLine();
-					if (map == null) {
-						throw new Exception("Invalid seeding map");
-					}
-					// xxxxx/xxxxx/xxxxx/xxxxx/xxxxx
-					// for each character in the string create a cell in the grid if it is an 'X' then the cell is alive otherwise it is dead
-					// for each '/' create a new row
-					int curRow = 0;
-					int curCol = 0;
-					foreach (char c in map)
-					{
-						switch (c) {
-							case '/':
-								// new row
-								curCol = 0;
-								curRow++;
-								break;
-
-							case 'X':
-							case 'x':
-								// live cell
-								seed[curRow, curCol] = new LiveCell();
-								curCol++;
-								break;
-
-							case '.':
-								// dead cell
-								seed[curRow, curCol] = new DeadCell();
-								curCol++;
-								break;
-
-							default:
-								throw new Exception("Invalid character in seeding map: " + c);
-						}
-					};
-					break;
-			}
+                case "3":
+                    seed = ParseSeeding(rows, cols);
+                    break;
+            }
 
 			Console.WriteLine("Starting Game of Life with rows: " + rows + " and cols: " + cols);
             var game = new GameOfLife(rows, cols);
@@ -95,5 +44,79 @@
 			}
             game.Start();
         }
-    }
+
+		static Cell[,]? RandomSeeding(int rows, int cols)
+        {
+            Cell[,] seed = new Cell[rows, cols];
+			Random random = new Random();
+			for (int i = 0; i < rows; i++)
+			{
+				for (int j = 0; j < cols; j++)
+				{
+					// 1 chance out of 3 for the cell to be alive
+					seed[i, j] = random.Next(3) == 0 ? new LiveCell() : new DeadCell();
+				}
+			}
+			return seed;
+        }
+
+        static Cell[,] ManualSeeding(int rows, int cols)
+        {
+            Cell[,] seed = new Cell[rows, cols];
+            Console.WriteLine("Enter the initial state of the grid:");
+            Console.WriteLine("Use 'X' for live cells and '.' for dead cells");
+            Console.WriteLine("Press enter after each row");
+
+            for (int i = 0; i < rows; i++) {
+                string? row = Console.ReadLine();
+                for (int j = 0; j < cols; j++) {
+                    if (row[j] == 'X' || row[j] == 'x') {
+                        // live cell
+                        seed[i, j] = new LiveCell();
+                    } else {
+                        // dead cell
+                        seed[i, j] = new DeadCell();
+                    }
+                }
+            }
+            return seed;
+        }
+
+        static Cell[,] ParseSeeding(int rows, int cols)
+        {
+            Console.WriteLine("Enter the seeding map:");
+            Cell[,] seed = new Cell[rows, cols];
+            string? map = Console.ReadLine();
+            if (map == null) {
+                throw new Exception("Invalid seeding map");
+            }
+            // xxxxx/xxxxx/xxxxx/xxxxx/xxxxx
+            int curRow = 0;
+            int curCol = 0;
+            foreach (char c in map)
+            {
+                switch (c) {
+                    case '/':
+                        curCol = 0;
+                        curRow++;
+                        break;
+
+                    case 'X':
+                    case 'x':
+                        seed[curRow, curCol] = new LiveCell();
+                        curCol++;
+                        break;
+
+                    case '.':
+                        seed[curRow, curCol] = new DeadCell();
+                        curCol++;
+                        break;
+
+                    default:
+                        throw new Exception("Invalid character in seeding map: " + c);
+                }
+            };
+            return seed;
+        }
+	}
 }

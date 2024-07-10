6f104f5a9585ff8bcef0d71401372832128e8bae
Herve Soulie <herve.soulie@vetup.com>
2024-07-10 12:58:01 +0200

    ajout parametres sizing de la grille

--- csharp/GameOfLife/GameOfLifeApp/Program.cs
+++ csharp/GameOfLife/GameOfLifeApp/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            var game = new GameOfLife(5, 5);
+			int rows = 5;
+			int cols = 5;
+
+			if (args.Length == 2) {
+				rows = int.Parse(args[0]);
+				cols = int.Parse(args[1]);
+			}
+
+			Console.WriteLine("Starting Game of Life with rows: " + rows + " and cols: " + cols);
+            var game = new GameOfLife(rows, cols);
+			Console.ReadLine();
             game.Start();
         }
     }

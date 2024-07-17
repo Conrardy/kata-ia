711f1d04966b6730c37dca678334d04a19c559b7
François R <144046669+fr-combo@users.noreply.github.com>
2024-07-17 13:34:09 +0200

    test: cell overcrowding rule

--- ts/game-of-life/src/evolve-cell.spec.ts
+++ ts/game-of-life/src/evolve-cell.spec.ts
@@ -1,25 +1,17 @@
 import { evolveCell } from "./evolve-cell";
 
 describe('evolveCell', () => {
-    const underpopulationTestCases = [
+    it.each([
         { cell: 1, neighbors: [0, 0, 0, 0, 0], expected: 0 },
         { cell: 1, neighbors: [1, 0, 0, 0, 0], expected: 0 }
-    ];
-
-    underpopulationTestCases.forEach(({ cell, neighbors, expected }) => {
-        it(`should return ${expected} if the cell is ${cell} and has neighbors ${neighbors}`, () => {
-            expect(evolveCell(cell, neighbors)).toBe(expected);
-        });
+    ])('should return $expected if the cell is $cell and has neighbors $neighbors', ({ cell, neighbors, expected }) => {
+        expect(evolveCell(cell, neighbors)).toBe(expected);
     });
 
-    const overcrowdingTestCases = [
+    it.each([
         { cell: 1, neighbors: [1, 1, 1, 1, 0], expected: 0 },
         { cell: 1, neighbors: [1, 1, 1, 1, 1], expected: 0 }
-    ];
-
-    overcrowdingTestCases.forEach(({ cell, neighbors, expected }) => {
-        it(`should return ${expected} if the cell is ${cell} and has neighbors ${neighbors}`, () => {
-            expect(evolveCell(cell, neighbors)).toBe(expected);
-        });
+    ])('should return $expected if the cell is $cell and has neighbors $neighbors', ({ cell, neighbors, expected }) => {
+        expect(evolveCell(cell, neighbors)).toBe(expected);
     });
 });

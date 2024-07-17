32980f9afdd4f29dceda75e68061675494c8e479
François R <144046669+fr-combo@users.noreply.github.com>
2024-07-17 13:42:01 +0200

    test: cell staying alive rule

--- ts/game-of-life/src/evolve-cell.spec.ts
+++ ts/game-of-life/src/evolve-cell.spec.ts
@@ -16,6 +16,13 @@ describe('evolveCell', () => {
     });
 
     it.each([
+        { cell: 1, neighbors: [1, 1, 0, 0, 0], expected: 1 },
+        { cell: 1, neighbors: [1, 1, 1, 0, 0], expected: 1 }
+    ])('should return $expected if the cell is $cell and has neighbors $neighbors', ({ cell, neighbors, expected }) => {
+        expect(evolveCell(cell, neighbors)).toBe(expected);
+    });
+
+    it.each([
         { cell: 0, neighbors: [1, 1, 1, 0, 0], expected: 1 }
     ])('should return $expected if the cell is $cell and has neighbors $neighbors', ({ cell, neighbors, expected }) => {
         expect(evolveCell(cell, neighbors)).toBe(expected);

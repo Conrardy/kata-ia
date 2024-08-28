3e6295b4ff5506b018d0d05947c3187c16c502e6
e.conrardy <conrarde51000@gmail.com>
2024-08-28 20:59:15 +0200

    rename migrations

--- csharp/TrainOffice/TrainOffice/Migrations/20240822173202_AddTableSummaries.cs
+++ csharp/TrainOffice/TrainOffice/Migrations/20240822173202_AddTableSummaries.cs
@@ -6,7 +6,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace TrainOffice.Migrations;
 
 /// <inheritdoc />
-public partial class InsertSummaries : Migration
+public partial class AddTableSummaries : Migration
 {
     /// <inheritdoc />
     protected override void Up(MigrationBuilder migrationBuilder)

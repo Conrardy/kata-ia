fd7d83b43a04896fb37b3f29172195bcd27576c6
e.conrardy <conrarde51000@gmail.com>
2024-08-28 19:30:45 +0200

    move seedData

---
+++ csharp/TrainOffice/TrainOffice/Configuration/Class.cs
@@ -0,0 +1,36 @@
+﻿using TrainOffice.Data;
+using TrainOffice.Models;
+
+namespace TrainOffice.Configuration;
+
+public static class ConfigureData
+{
+    public static void SeedInMemoryDb(this WebApplication app, IConfiguration configuration)
+    {
+        var databaseProvider = configuration.GetValue<string>("DatabaseProvider");
+        if (databaseProvider == ConfigurePersistences.InMemoryDb)
+        {
+            using var scope = app.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var context = services.GetRequiredService<ApplicationDbContext>();
+            context.Database.EnsureCreated();
+            AddTestData(context);
+        }
+    }
+
+    private static void AddTestData(ApplicationDbContext context)
+    {
+        var summaries = new List<Summary>
+        {
+            new() { Content = "Mild" },
+            new() { Content = "Hot" },
+            new() { Content = "Cold" },
+            new() { Content = "Warm" },
+            new() { Content = "Freezing" },
+            new() { Content = "Cool" },
+        };
+
+        context.Summaries.AddRange(summaries);
+        context.SaveChanges();
+    }
+}
--- csharp/TrainOffice/TrainOffice/Program.cs
+++ csharp/TrainOffice/TrainOffice/Program.cs
@@ -1,6 +1,4 @@
 using TrainOffice.Configuration;
-using TrainOffice.Data;
-using TrainOffice.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +14,7 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
-SeedInMemoryDb(configuration, app);
+app.SeedInMemoryDb(configuration);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -33,37 +31,8 @@ app.MapGet(
     "/",
     async context =>
     {
-        await context.Response.SendFileAsync("src/index.html");
+        await context.Response.SendFileAsync("Src/index.html");
     }
 );
 
 app.Run();
-
-static void AddTestData(ApplicationDbContext context)
-{
-    var summaries = new List<Summary>
-    {
-        new() { Content = "Mild" },
-        new() { Content = "Hot" },
-        new() { Content = "Cold" },
-        new() { Content = "Warm" },
-        new() { Content = "Freezing" },
-        new() { Content = "Cool" },
-    };
-
-    context.Summaries.AddRange(summaries);
-    context.SaveChanges();
-}
-
-static void SeedInMemoryDb(ConfigurationManager configuration, WebApplication app)
-{
-    var databaseProvider = configuration.GetValue<string>("DatabaseProvider");
-    if (databaseProvider == ConfigurePersistences.InMemoryDb)
-    {
-        using var scope = app.Services.CreateScope();
-        var services = scope.ServiceProvider;
-        var context = services.GetRequiredService<ApplicationDbContext>();
-        context.Database.EnsureCreated();
-        AddTestData(context);
-    }
-}
